47a9290dedbd63aeea837c8f16258a5ad560ee23
AratiBudihale <aratibudihale@gmail.com>
2024-05-08 15:01:39 +0530

    show birthdays

---
+++ src/Components/Board.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import './board.css';
+import List from './list'
+
+
+const Info = [
+    {
+        img : "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name : "Arati Budihale",
+        empId:1,
+        birthday : "1999-05-08"
+    },
+    {
+      img : "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      name : "Preeti Shitole",
+      empId:2,
+      birthday : "1995-05-08"
+  },
+    {
+        img : "https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name : "Miller Wright",
+        empId:3,
+        birthday : "1998-05-29"
+    },
+    {
+        img : "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name : "Tara Kelly",
+        empId:4,
+        birthday : "1991-02-02"
+    },
+    {
+      img : "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      empId:5,
+        name : "Freddie Watson", birthday : "1999-10-15"
+    },
+    {
+      img : "https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      empId:6,
+        name : "Brianna Baker", birthday : "1994-07-05"
+    },
+    {
+      img : "https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name : "Brad Myers",
+        empId:7,
+         birthday : "1997-01-28"
+    }
+  ]
+
+export default function Board() {
+  return (
+      <main id='site-main'>
+          <h1 className="text-dark title">Todays Birthday</h1>
+
+          <div className="board">
+                <List info={Today(Info)}></List>
+                <h2 className='upcoming text-dark'>Upcoming Birthday</h2>
+                <List info={Upcoming(Info, 2)} upcoming={true}></List>
+          </div>
+      </main>
+  );
+}
+
+function Today(person){
+    let currentDay = new Date().getDate();
+    let currentMonth = new Date().getMonth();
+
+    let filter = person.filter(data => {
+        let day = new Date(data.birthday).getDate();
+        let month = new Date(data.birthday).getMonth();
+
+        return currentDay == day && currentMonth == month;
+    })
+    return filter;
+}
+
+
+// upcoming birthdays
+function Upcoming(person, toMonth){
+    let currentMonth = new Date().getMonth();
+    let currentDay = new Date().getDate();
+
+    let filter =person.filter(data => {
+        let month = new Date(data.birthday).getMonth();
+        let day = new Date(data.birthday).getDate();
+
+        if (currentDay == day) return;
+        return month >= currentMonth && month <= currentMonth + toMonth;
+    })
+
+    return filter;
+
+}
